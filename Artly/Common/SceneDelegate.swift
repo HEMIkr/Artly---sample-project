54778407815c24497105fb540ce7fcaec96b0152
Aleksander Wędrychowski <aleksander.wedrychowski@alior.pl>
2021-01-12 19:20:55 +0100

    Basic project organization + swiftlint

---
+++ Artly/Common/SceneDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  SceneDelegate.swift
+//  Artly
+//
+//  Created by Aleksander Wędrychowski on 12/01/2021.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+}

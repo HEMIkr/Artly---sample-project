117686e7176c64a0c664ff49e526b33f8d93f484
Aleksander Wędrychowski <aleksander.wedrychowski@alior.pl>
2021-01-12 19:08:18 +0100

    Initial commit

---
+++ Artly/Artly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Artly
+//
+//  Created by Aleksander Wędrychowski on 12/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
